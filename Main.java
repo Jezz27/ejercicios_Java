f8fcaba02b3cc48e20e00765cc009fdee6bd9931
Jezz27 <jmzapatal@ufpso.edu.co>
2024-10-15 07:17:07 -0500

    xxx

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)){
-
-        
         System.out.println("Ingrese numero a,b y c");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
